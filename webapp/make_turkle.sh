15bfb316fc5d22e8d052e1310e63ad4658e412b3
esteng <elias.stengel@gmail.com>
2024-05-02 17:48:28 -0400

    clean up template

--- webapp/make_turkle.sh
+++ webapp/make_turkle.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# npm run-script build
+# run deploy script 
+npm run deploy
 
+# add css 
 echo "<style type=\"text/css\">" > anno-app-turkle.html
 cat build/static/css/main.*.css >> anno-app-turkle.html
 echo "</style>" >> anno-app-turkle.html
@@ -8,16 +10,11 @@ echo "</style>" >> anno-app-turkle.html
 cat skeleton.html >> anno-app-turkle.html
 
 ## cat the scripts into the html
-main_file=$(ls ~/annotation_hit/webapp/build/static/js/main.*.js)
-echo "<script type=\"text/javascript\" src=\"https://esteng.github.io/annotation_hit/${main_file}\">" >> anno-app-turkle.html
-# cat build/static/js/*.chunk.js >> anno-app-turkle.html
-# echo "" >> anno-app-turkle.html
-# cat build/static/js/main.*.js >> anno-app-turkle.html
-# echo "" >> anno-app-turkle.html
+main_file=$(basename $(ls build/static/js/main.*.js))
 
-# cat build/static/js/bundle.js >> anno-app-turkle.html
+echo ${main_file} 
+echo "<script type=\"text/javascript\" src=\"https://esteng.github.io/annotation_hit/static/js/${main_file}\">" >> anno-app-turkle.html
 echo "" >> anno-app-turkle.html
-
 echo "</script>" >> anno-app-turkle.html
-#
+
 echo "DONE"
